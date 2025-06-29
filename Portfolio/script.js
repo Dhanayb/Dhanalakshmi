0cf37d5e6b19bccc76b70835702940aafab8024e
Dhanayb <dhanalakshmiyerninti@gmail.com>
2025-06-30 00:55:37 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+<script>
+      // Update your existing mobile menu toggle script
+      menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        // Toggle menu icon animation
+        menuToggle.classList.toggle("active");
+      });
+    </script>
